8db985041be647dc0b7e2055737d77dcf6b70719
Serdar Tahir Kabaoglu <skabaoglu@newstore.com>
2020-06-07 00:40:41 +0200

    add sample files

--- cmd/sample/resources.go
+++ cmd/sample/resources.go
@@ -10,60 +10,60 @@ var resources [][]string
 func resourceList() [][]string {
 	if resources == nil {
 		resources = [][]string{
-			{"bindings", "Binding"},
-			{"componentstatuses", "cs", "ComponentStatus"},
-			{"configmaps", "cm", "ConfigMap"},
-			{"endpoints", "ep", "Endpoints"},
-			{"events", "ev", "Event"},
-			{"limitranges", "limits", "LimitRange"},
-			{"namespaces", "ns", "Namespace"},
-			{"nodes", "no", "Node"},
-			{"persistentvolumeclaims", "pvc", "PersistentVolumeClaim"},
-			{"persistentvolumes", "pv", "PersistentVolume"},
-			{"pods", "po", "Pod"},
-			{"podtemplates", "PodTemplate"},
-			{"replicationcontrollers", "rc", "ReplicationController"},
-			{"resourcequotas", "quota", "ResourceQuota"},
-			{"secrets", "Secret"},
-			{"serviceaccounts", "sa", "ServiceAccount"},
-			{"services", "svc", "Service"},
-			{"mutatingwebhookconfigurations", "MutatingWebhookConfiguration"},
-			{"validatingwebhookconfigurations", "ValidatingWebhookConfiguration"},
-			{"customresourcedefinitions", "crd", "crds", "CustomResourceDefinition"},
-			{"apiservices", "APIService"},
-			{"controllerrevisions", "ControllerRevision"},
-			{"daemonsets", "ds", "DaemonSet"},
-			{"deployments", "deploy", "Deployment"},
-			{"replicasets", "rs", "ReplicaSet"},
-			{"statefulsets", "sts", "StatefulSet"},
-			{"tokenreviews", "TokenReview"},
-			{"localsubjectaccessreviews", "LocalSubjectAccessReview"},
-			{"selfsubjectaccessreviews", "SelfSubjectAccessReview"},
-			{"selfsubjectrulesreviews", "SelfSubjectRulesReview"},
-			{"subjectaccessreviews", "SubjectAccessReview"},
-			{"horizontalpodautoscalers", "hpa", "HorizontalPodAutoscaler"},
-			{"cronjobs", "cj", "CronJob"},
-			{"jobs", "Job"},
-			{"certificatesigningrequests", "csr", "CertificateSigningRequest"},
-			{"leases", "Lease"},
-			{"endpointslices", "EndpointSlice"},
-			{"events", "ev", "Event"},
-			{"ingresses", "ing", "Ingress"},
-			{"ingressclasses", "IngressClass"},
-			{"ingresses", "ing", "Ingress"},
-			{"networkpolicies", "netpol", "NetworkPolicy"},
-			{"runtimeclasses", "RuntimeClass"},
-			{"poddisruptionbudgets", "pdb", "PodDisruptionBudget"},
-			{"podsecuritypolicies", "psp", "PodSecurityPolicy"},
-			{"clusterrolebindings", "ClusterRoleBinding"},
-			{"clusterroles", "ClusterRole"},
-			{"rolebindings", "RoleBinding"},
-			{"roles", "Role"},
-			{"priorityclasses", "pc", "PriorityClass"},
-			{"csidrivers", "CSIDriver"},
-			{"csinodes", "CSINode"},
-			{"storageclasses", "sc", "StorageClass"},
-			{"volumeattachments", "VolumeAttachment"},
+			{"bindings", "Binding"},                                                  // TODO no example available
+			{"componentstatuses", "cs", "ComponentStatus"},                           // TODO no example available
+			{"configmaps", "cm", "ConfigMap"},                                        // Done
+			{"endpoints", "ep", "Endpoints"},                                         // Done
+			{"events", "ev", "Event"},                                                // TODO no example available
+			{"limitranges", "limits", "LimitRange"},                                  // Done
+			{"namespaces", "ns", "Namespace"},                                        // Done
+			{"nodes", "no", "Node"},                                                  // Done
+			{"persistentvolumeclaims", "pvc", "PersistentVolumeClaim"},               // Done
+			{"persistentvolumes", "pv", "PersistentVolume"},                          // Done
+			{"pods", "po", "Pod"},                                                    // Done
+			{"podtemplates", "PodTemplate"},                                          // TODO no example available
+			{"replicationcontrollers", "rc", "ReplicationController"},                // Done
+			{"resourcequotas", "quota", "ResourceQuota"},                             // Done NOTE example is a list of resources
+			{"secrets", "Secret"},                                                    // Done
+			{"serviceaccounts", "sa", "ServiceAccount"},                              // Done
+			{"services", "svc", "Service"},                                           // Done
+			{"mutatingwebhookconfigurations", "MutatingWebhookConfiguration"},        // TODO check if it could be improved
+			{"validatingwebhookconfigurations", "ValidatingWebhookConfiguration"},    // TODO check if it could be improved
+			{"customresourcedefinitions", "crd", "crds", "CustomResourceDefinition"}, // TODO check if it could be improved
+			{"apiservices", "APIService"},                                            // TODO no example available
+			{"controllerrevisions", "ControllerRevision"},                            // TODO no example available
+			{"daemonsets", "ds", "DaemonSet"},                                        // Done
+			{"deployments", "deploy", "Deployment"},                                  // Done
+			{"replicasets", "rs", "ReplicaSet"},                                      // Done
+			{"statefulsets", "sts", "StatefulSet"},                                   // Done NOTE double yaml
+			{"tokenreviews", "TokenReview"},                                          // WON'T DO  used between services
+			{"localsubjectaccessreviews", "LocalSubjectAccessReview"},                // WON'T DO use kubectl auth can-i instead
+			{"selfsubjectaccessreviews", "SelfSubjectAccessReview"},                  // WON'T DO use kubectl auth can-i instead
+			{"selfsubjectrulesreviews", "SelfSubjectRulesReview"},                    // WON'T DO use kubectl auth can-i instead
+			{"subjectaccessreviews", "SubjectAccessReview"},                          // WON'T DO use kubectl auth can-i instead
+			{"horizontalpodautoscalers", "hpa", "HorizontalPodAutoscaler"},           // Done
+			{"cronjobs", "cj", "CronJob"},                                            // Done
+			{"jobs", "Job"},                                                          // Done
+			{"certificatesigningrequests", "csr", "CertificateSigningRequest"},       // Done
+			{"leases", "Lease"},                                                      // TODO no example available
+			{"endpointslices", "EndpointSlice"},                                      // Done
+			{"events", "ev", "Event"},                                                // TODO no example available
+			{"ingresses", "ing", "Ingress"},                                          // TODO extensions api group
+			{"ingressclasses", "IngressClass"},                                       // Done
+			{"ingresses", "ing", "Ingress"},                                          // Done // networking.k8s.io api group
+			{"networkpolicies", "netpol", "NetworkPolicy"},                           // Done
+			{"runtimeclasses", "RuntimeClass"},                                       // Done
+			{"poddisruptionbudgets", "pdb", "PodDisruptionBudget"},                   // Done
+			{"podsecuritypolicies", "psp", "PodSecurityPolicy"},                      // Done
+			{"clusterrolebindings", "ClusterRoleBinding"},                            // Done
+			{"clusterroles", "ClusterRole"},                                          // Done
+			{"rolebindings", "RoleBinding"},                                          // Done
+			{"roles", "Role"},                                                        // Done
+			{"priorityclasses", "pc", "PriorityClass"},                               // Done
+			{"csidrivers", "CSIDriver"},                                              // TODO no simple example available
+			{"csinodes", "CSINode"},                                                  // TODO no simple example available
+			{"storageclasses", "sc", "StorageClass"},                                 // Done NOTE multiple examples at https://kubernetes.io/docs/concepts/storage/storage-classes/
+			{"volumeattachments", "VolumeAttachment"},                                // TODO no example available
 		}
 	}
 
